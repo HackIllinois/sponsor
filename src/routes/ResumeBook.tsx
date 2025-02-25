4dccd010e02b3bb70142ed8ba0e4adf4a39f2e3d
Jacob <edleyjacob@gmail.com>
2025-02-25 01:01:32 -0600

    redirect to login if not authenticated

--- src/routes/ResumeBook.tsx
+++ src/routes/ResumeBook.tsx
@@ -406,6 +406,9 @@ export function ResumeBook() {
     }
 
     useEffect(() => {
+        if (!localStorage.getItem("jwt")) {
+            window.location.href = "/login";
+        }
         // const handleResize = () => {
         //     setIsMobile(window.innerWidth < 550);
         // };
